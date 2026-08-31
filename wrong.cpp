1951b14d543e956b90140b3cd2a0a2d506622804
*Gaurav-Yadav-05* <*gauravrao111222333@gmail.com*>
2026-08-31 22:04:48 +0530

    testing workflow

---
+++ wrong.cpp
@@ -0,0 +1,8 @@
+cout<<"get the value of a: ";
+    cin>>a;
+    cout<<"get the value of b: ";
+    cin>>b;
+    cout<<"get the value of c: ";
+    cin>>c;
+    cout<<"get the value of d: ";
+    cin>>d;
